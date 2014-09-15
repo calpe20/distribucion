bd187f86c842f8f10fadeb0734649bc074fa32fd
Calpe20 <calpe20@gmail.com>
2014-09-15 17:31:55 -0500

    Cargando

--- app.py
+++ app.py
@@ -22,9 +22,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
-
-@app.route('/usuario/<name>')
-def usuarios(name=None):
 	
 if __name__=='__main__':
 	app.run(host='0.0.0.0', debug=True)
