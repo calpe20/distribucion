f65c35400ef6d15583bb35becc6990cb0e1e0402
calpe20 <calpe20@gmail.com>
2014-09-15 23:32:35 -0500

    Mapas

--- app.py
+++ app.py
@@ -2,26 +2,34 @@ from flask import Flask
 from flask import render_template
 import MySQLdb
 
-db_host='192.168.1.41'
-usuario = 'root'
-clave	= 'root'
-basedato= 'cfageren_prueba'
+#db_host='192.168.1.41'
+#usuario = 'root'
+#clave	= 'root'
+#basedato= 'cfageren_prueba'
 
-db = MySQLdb.connect(host=db_host, 
-	user= usuario, 
-	passwd=clave, 
-	db=basedato)
+#db = MySQLdb.connect(host=db_host, 
+#	user= usuario, 
+#	passwd=clave, 
+#	db=basedato)
 	
-cursor = db.cursor()
+#cursor = db.cursor()
 
-query = 'SELECT * FROM usuario'
+#query = 'SELECT * FROM usuario'
 
-cursor.execute(query)
+#cursor.execute(query)
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
 	return render_template('index.html')
-	
+
+@app.route('/mapa')
+def mapa():
+	saludos ='Esperando'
+	zoom = 15
+	coordenadas = "-8.377651, -74.543964"
+	return render_template('mapa.html', saludo = saludos, coordenadas=coordenadas, zoom=zoom)
+
 if __name__=='__main__':
 	app.run(host='0.0.0.0', debug=True)
